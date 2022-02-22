fd30dca409d06924b958c4185528b05db292f5f8
Totskyi Vitalii <totskiyvitaliy231000@gmail.com>
2022-02-22 04:08:01 +0200

    bug fixed

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Fab 25, 2022',
+  birthdate: 'Feb 25, 2022',
   name: 'Bruh<3'
 };
 
