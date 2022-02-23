06d8003192fce39098e7d2175bae7307cda0f7ab
Totskyi Vitalii <totskiyvitaliy231000@gmail.com>
2022-02-23 02:47:19 +0200

    time adapted

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+let countDown = new Date(`${config.birthdate} 00:00:00 GMT-0500`).getTime();
 x = setInterval(function() {
   let now = new Date().getTime(),
     distance = countDown - now;
